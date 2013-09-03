50cf534f2e9ddfa98e1237a23d4bff9097140af3
James Reeves <jreeves@weavejester.com>
2013-09-03 16:18:07 +0100

    Added timing test

--- test/crypto/equality_test.clj
+++ test/crypto/equality_test.clj
@@ -2,6 +2,15 @@
   (:use clojure.test
         crypto.equality))
 
+(defmacro time-expr [& form]
+  `(let [t# (System/nanoTime)]
+     ~@form
+     (- (System/nanoTime) t#)))
+
+(defn approx-eq [x y tolerance]
+  (<= (/ (Math/abs (- x y)) x)
+      tolerance))
+
 (deftest eq?-test
   (testing "equality"
     (is (eq? "foo" "foo"))
@@ -12,4 +21,11 @@
     (is (not (eq? "foo" "bar")))
     (is (not (eq? "foo" "foob")))
     (is (not (eq? "foob" "foo")))
-    (is (not (eq? [1 2 3] [3 2 1])))))
+    (is (not (eq? [1 2 3] [3 2 1]))))
+  (testing "timing"
+    (let [xs (int-array (repeat 1000 3))
+          ys (int-array (repeat 1000 0))
+          zs (int-array (concat (repeat 999 3) [0]))]
+      (is (approx-eq (time-expr (dotimes [_ 1000] (eq? xs ys)))
+                     (time-expr (dotimes [_ 1000] (eq? xs zs)))
+                     0.2)))))
