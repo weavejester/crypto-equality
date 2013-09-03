f7528f92ac3e00eef7b604cba94b1020795e30e9
James Reeves <jreeves@weavejester.com>
2013-09-03 16:03:53 +0100

    Added eq? function from Ring's secure-compare

--- src/crypto/equality.clj
+++ src/crypto/equality.clj
@@ -1,1 +1,7 @@
 (ns crypto.equality)
+
+(defn eq? [a b]
+  (let [a (map int a), b (map int b)]
+    (if (and a b (= (count a) (count b)))
+      (zero? (reduce bit-or (map bit-xor a b)))
+      false)))
--- test/crypto/equality_test.clj
+++ test/crypto/equality_test.clj
@@ -1,7 +1,15 @@
 (ns crypto.equality-test
-  (:use clojure.test)
-  (:require crypto.equality))
+  (:use clojure.test
+        crypto.equality))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest eq?-test
+  (testing "equality"
+    (is (eq? "foo" "foo"))
+    (is (eq? [1 2 3] [1 2 3]))
+    (let [xs (take 1000 (repeatedly #(rand-int 1000000)))]
+      (is (eq? (vec xs) (vec xs)))))
+  (testing "inequality"
+    (is (not (eq? "foo" "bar")))
+    (is (not (eq? "foo" "foob")))
+    (is (not (eq? "foob" "foo")))
+    (is (not (eq? [1 2 3] [3 2 1])))))
