7ad52eecef52e912f0e6cab0804630be052ceefd
James Reeves <jreeves@weavejester.com>
2014-07-08 18:57:57 +0100

    Interlace timing tests to minimise interference

--- test/crypto/equality_test.clj
+++ test/crypto/equality_test.clj
@@ -25,7 +25,11 @@
   (testing "timing"
     (let [xs (int-array (repeat 1000 3))
           ys (int-array (repeat 1000 0))
-          zs (int-array (concat (repeat 999 3) [0]))]
-      (is (approx-eq (time-expr (dotimes [_ 1000] (eq? xs ys)))
-                     (time-expr (dotimes [_ 1000] (eq? xs zs)))
-                     0.2)))))
+          zs (int-array (concat (repeat 999 3) [0]))
+          ts (loop [i 0, t0 0, t1 0]
+               (if (< i 1000)
+                 (recur (inc i)
+                        (+ t0 (time-expr (eq? xs ys)))
+                        (+ t1 (time-expr (eq? xs zs))))
+                 [t0 t1]))]
+      (is (approx-eq (ts 0) (ts 1) 0.2)))))
