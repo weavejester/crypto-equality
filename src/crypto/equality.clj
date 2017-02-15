04b9be236fcf02dfdebad99267fc66c98912c9e0
Milt Reder <milt@yetanalytics.com>
2017-02-15 16:49:44 -0500

    handle zero length strings/sequences

--- src/crypto/equality.clj
+++ src/crypto/equality.clj
@@ -6,7 +6,9 @@
   protects against timing attacks. Note that this does not prevent an attacker
   from discovering the *length* of the data being compared."
   [a b]
-  (let [a (map int a), b (map int b)]
-    (if (and a b (= (count a) (count b)))
+  (let [a (seq (map int a)), b (seq (map int b))]
+    (cond
+      (= nil a b) true
+      (and a b (= (count a) (count b)))
       (zero? (reduce bit-or (map bit-xor a b)))
-      false)))
+      :else false)))
